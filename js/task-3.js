82ec711dc9a26928d9509ffd51b24f13ada98115
Hanna Ilkiv <shov.guitar@gmail.com>
2026-02-03 16:55:49 +0100

    update js task 3

--- js/task-3.js
+++ js/task-3.js
@@ -10,7 +10,7 @@ const profile = {
     },
 
     getInfo() {
-        return `${this.username} has ${this.playTime} active hours`;
+        return `${this.username} has ${this.playTime} active hours!`;
      },
 };
 
